da5680c27f317d9e2fc290287b33bad2fb41a009
1047473086 <1047473086@qq.com>
2024-02-02 17:51:46 +0100

    add staff dashboard

--- ui/script.js
+++ ui/script.js
@@ -16,11 +16,25 @@ function login() {
     })
     .then(data => {
         console.log(data);
-        if (data.redirect) {
+        if (data.redirect == "/user_dashboard") {
+            console.log("user");
             document.getElementById('login-section').style.display = 'none';
             document.getElementById('books-section').style.display = 'block';
             fetchBooks();
-        } else {
+        }
+        else if (data.redirect == "/admin_dashboard") {
+            console.log("admin");
+            document.getElementById('login-section').style.display = 'none';
+            document.getElementById('admin-section').style.display = 'block';
+            fetchBooks();
+        } 
+        else if (data.redirect == "/staff_dashboard") {
+            console.log("staff");
+            document.getElementById('login-section').style.display = 'none';
+            document.getElementById('staff-section').style.display = 'block';
+            fetchBooksStaff();            
+        }
+        else {
             alert('Login failed');
         }
     });
@@ -37,6 +51,23 @@ function fetchBooks() {
             item.textContent = `${book.title} by ${book.author} (${book.genre})`;
             booksList.appendChild(item);
         });
+    });
+}
+
+
+function fetchBooksStaff() {
+    console.log("fetchBooksStaff");
+    fetch('http://localhost:5000/inventory/api/v1.0/books') // Adjust API URL
+    .then(response => response.json())
+    .then(data => {
+        const booksList = document.getElementById('books-liststaff');
+        booksList.innerHTML = '';
+        data.books.forEach(book => {
+            const item = document.createElement('li');
+            item.textContent = `${book.title} by ${book.author} (${book.genre}), ${book.quantity} in stock`;
+            booksList.appendChild(item);
+        });
+        console.log(booksList);
     });
 }
 
